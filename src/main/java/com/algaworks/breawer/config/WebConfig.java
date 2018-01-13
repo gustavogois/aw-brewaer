3f39a044df25e860bc248d73d820f91c92314b9d
Gustavo Gois <gustavo.gois@gmail.com>
2018-01-13 09:33:14 +0000

    configurando a ViewResolver com Thymeleaf

--- src/main/java/com/algaworks/breawer/config/WebConfig.java
+++ src/main/java/com/algaworks/breawer/config/WebConfig.java
@@ -1,9 +1,20 @@
 package com.algaworks.breawer.config;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import com.algaworks.breawer.controller.CervejasController;
 
@@ -15,6 +26,40 @@ import com.algaworks.breawer.controller.CervejasController;
 @EnableWebMvc
 public class WebConfig 
 	// Podemos estender adaptadores para facilitar nossa configuração
-	extends WebMvcConfigurerAdapter {
+	extends WebMvcConfigurerAdapter 
+	// Para permitir utilizar o application context 
+	implements ApplicationContextAware {
+	
+	private ApplicationContext applicationContext;
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+
+	@Bean
+	public ViewResolver viewResolver() {
+		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+		resolver.setTemplateEngine(templateEngine());
+		resolver.setCharacterEncoding("UTF-8");
+		return resolver;
+	}
+
+	@Bean
+	public TemplateEngine templateEngine() {
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setEnableSpringELCompiler(true);
+		engine.setTemplateResolver(templateResolver());
+		return engine;
+	}
+
+	private ITemplateResolver templateResolver() {
+		SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+		resolver.setApplicationContext(applicationContext);
+		resolver.setPrefix("classpath:/templates/");
+		resolver.setSuffix(".html");
+		resolver.setTemplateMode(TemplateMode.HTML);
+		return resolver;
+	}
 
 }
